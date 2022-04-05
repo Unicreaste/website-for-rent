071f110a3de3837a15ed39ca95e0b8ef5c172732
STEPA <stepa03102007@gmail.com>
2022-04-05 17:15:06 +0300

    Изменена база даннных с пользователями

--- Пример/additional/data/users.py
+++ Пример/additional/data/users.py
@@ -1,4 +1,3 @@
-import datetime
 import sqlalchemy
 from flask_login import UserMixin
 from sqlalchemy import orm
@@ -13,13 +12,13 @@ class User(SqlAlchemyBase, UserMixin):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
-    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    tel_num = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    modifed_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    avatar = sqlalchemy.Column(
+        sqlalchemy.StdImageField(variations={"thumbnail": {'height': 100, 'width': 100}}), nullable=False)
 
     jobs = orm.relation("Jobs", back_populates='user')
     departments = orm.relation("Department", back_populates='user')
